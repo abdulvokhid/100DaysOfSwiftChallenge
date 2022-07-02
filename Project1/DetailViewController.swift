f44063f73e5d5e3f5b8a0a089779f285cbab80e4
abdulvokhid <abdulvokhid.azimov97@gmail.com>
2022-07-02 11:06:37 +0200

    Day 17-Swift Challenge Project1, part two Building a detail screen

---
+++ Project1/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Abdulvokhid Azimov on 02.07.2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
