5f92fb277f89ef68db95c4bb0522eb31ae7af185
abdulvokhid <abdulvokhid.azimov97@gmail.com>
2022-07-11 20:33:28 +0200

    #Day-22 Swift challenge Project3, created UIActivityViewController explained

--- Project3/Project3/DetailViewController.swift
+++ Project3/Project3/DetailViewController.swift
@@ -18,8 +18,9 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
 
         title = " Picture \(selectedPictureNumber!) of \(totalPicture!)"
-        
         navigationItem.largeTitleDisplayMode = .never
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
         }
@@ -33,4 +34,13 @@ class DetailViewController: UIViewController {
         navigationController?.hidesBarsOnTap = false
     }
     
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
 }
