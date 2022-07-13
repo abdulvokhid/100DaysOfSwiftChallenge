a97053c9326ca16cab7d5cd8f07983dc4f4373e0
abdulvokhid <abdulvokhid.azimov97@gmail.com>
2022-07-13 17:30:14 +0200

    #Day-23 Milestone: Project1-3, Challenge-1, created setting up app template

---
+++ Challenge_1 For P_1,2,3/Challenge_1 For P_1,2,3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Challenge_1 For P_1,2,3
+//
+//  Created by Abdulvokhid Azimov on 13.07.2022.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
