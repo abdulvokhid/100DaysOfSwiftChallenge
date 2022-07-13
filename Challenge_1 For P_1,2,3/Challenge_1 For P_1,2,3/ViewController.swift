137d0cd775c3f0088ee837ca86206ea46db5c5ca
abdulvokhid <abdulvokhid.azimov97@gmail.com>
2022-07-13 18:10:38 +0200

    #Day-23 Milestone: Project1-3, created Challenge-2 Load the list of available flags from the app bundle. You can type them directly into the code if you want, but its preferable not to.

--- Challenge_1 For P_1,2,3/Challenge_1 For P_1,2,3/ViewController.swift
+++ Challenge_1 For P_1,2,3/Challenge_1 For P_1,2,3/ViewController.swift
@@ -8,10 +8,24 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    var flags = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //challenge-2 Load the list of available flags from the app bundle. You can type them directly into the code if you want, but its preferable not to.
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasSuffix(".png"){
+//                flags.append(item)
+                print(item)
+            }
+        }
+        //print(flags)
     }
 
 
