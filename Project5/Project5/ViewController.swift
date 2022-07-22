ee07c44a56b09a83e2283aa2e06701831267dc83
abdulvokhid <abdulvokhid.azimov97@gmail.com>
2022-07-22 09:28:49 +0200

    #Day-27 Swift Challenge Project5 part one, #created Setting up the project

---
+++ Project5/Project5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by Abdulvokhid Azimov on 22.07.2022.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
