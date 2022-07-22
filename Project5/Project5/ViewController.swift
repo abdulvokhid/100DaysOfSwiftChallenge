d5766c7f9d6ac5965c335991ad290939136d0c67
abdulvokhid <abdulvokhid.azimov97@gmail.com>
2022-07-22 09:45:22 +0200

    #Day-27 Swift Challenge Project5 part one, #created Reading from disk: contentsOfFile

--- Project5/Project5/ViewController.swift
+++ Project5/Project5/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UITableViewController {
 
+    var allWords = [String]()
+    var usedWords = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let startWordsUrl = Bundle.main.url(forResource: "start", withExtension: "txt"){
+            if let startWords = try? String(contentsOf: startWordsUrl) {
+                allWords = startWords.components(separatedBy: "\n")
+            } else {
+                allWords = ["silkworm"]
+            }
+        }
+        
+        if allWords.isEmpty {
+            allWords = ["silkworm"]
+        }
+        
+        startGame()
     }
 
-
+    func startGame() {
+        title = allWords.randomElement() // sets title to random array
+        usedWords.removeAll(keepingCapacity: true) //removes all words thats used in array
+        tableView.reloadData() //it calls reloadData method to reloads all data, its good for upgrading next level
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return usedWords.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
+    
 }
 
